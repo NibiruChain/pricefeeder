d9a3cdba53e9f925cc805eb2f451646bc5f107e2
Jonathan Gimeno <jgimeno@gmail.com>
2023-02-01 14:15:57 +0100

    revert map

--- feeder/priceprovider/sources/binance_test.go
+++ feeder/priceprovider/sources/binance_test.go
@@ -3,14 +3,13 @@ package sources
 import (
 	"testing"
 
-	"github.com/NibiruChain/nibiru/x/common/set"
 	"github.com/NibiruChain/price-feeder/types"
 	"github.com/stretchr/testify/require"
 )
 
 func TestBinanceSource(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
-		rawPrices, err := BinancePriceUpdate(set.New[types.Symbol]("BTCUSD", "ETHUSD"))
+		rawPrices, err := BinancePriceUpdate([]types.Symbol{"BTCUSD", "ETHUSD"})
 		require.NoError(t, err)
 		require.Equal(t, 2, len(rawPrices))
 		require.NotZero(t, rawPrices["BTCUSD"])
