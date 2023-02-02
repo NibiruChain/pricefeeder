458cd5a7512d69186d15c692f71268725ad4da5b
Jonathan Gimeno <jgimeno@gmail.com>
2023-02-02 21:33:44 +0100

    add check for config without api key

--- feeder/priceprovider/sources/coingecko_test.go
+++ feeder/priceprovider/sources/coingecko_test.go
@@ -55,4 +55,26 @@ func TestCoingeckoWithConfig(t *testing.T) {
 		require.Equal(t, rawPrices["bitcoin"], 23829.0)
 		require.Equal(t, rawPrices["ethereum"], 1676.85)
 	})
+
+	t.Run("providing config without api_key ignores and calls free endpoint", func(t *testing.T) {
+		httpmock.RegisterResponder(
+			"GET", FreeLink+"simple/price?ids=bitcoin%2Cethereum&vs_currencies=usd",
+			httpmock.NewStringResponder(200, "{\"bitcoin\":{\"usd\":23829},\"ethereum\":{\"usd\":1676.85}}"),
+		)
+
+		options := "{\"not_api_key\": \"1234567890\"}"
+		jsonOptions := json.RawMessage{}
+		err := json.Unmarshal([]byte(options), &jsonOptions)
+		require.NoError(t, err)
+
+		rawPrices, err := CoingeckoPriceUpdate(jsonOptions)([]types.Symbol{
+			"bitcoin",
+			"ethereum",
+		})
+		require.NoError(t, err)
+
+		require.Equal(t, 2, len(rawPrices))
+		require.Equal(t, rawPrices["bitcoin"], 23829.0)
+		require.Equal(t, rawPrices["ethereum"], 1676.85)
+	})
 }
