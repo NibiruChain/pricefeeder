12ff11e4e46ca453215105ef7765460a0754666b
anywhy <yangdaixing168@gmail.com>
2023-02-28 22:59:43 +0800

    update priceprovider add gateio

--- feeder/priceprovider/priceprovider.go
+++ feeder/priceprovider/priceprovider.go
@@ -44,6 +44,8 @@ func NewPriceProvider(
 		source = sources.NewTickSource(symbolsFromPairToSymbolMapping(pairToSymbolMap), sources.BinancePriceUpdate, logger)
 	case sources.Coingecko:
 		source = sources.NewTickSource(symbolsFromPairToSymbolMapping(pairToSymbolMap), sources.CoingeckoPriceUpdate(config), logger)
+	case sources.GateIo:
+		source = sources.NewTickSource(symbolsFromPairToSymbolMapping(pairToSymbolMap), sources.GateIoPriceUpdate, logger)
 	default:
 		panic("unknown price provider: " + sourceName)
 	}
