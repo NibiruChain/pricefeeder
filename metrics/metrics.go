3bcfcd4cbcb77bacf62692b588963ca9fa1da664
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2023-11-24 10:02:58 -0500

    feat(metrics): add price source latency metrics

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -9,3 +9,9 @@ var NumVotingPeriods = promauto.NewCounter(prometheus.CounterOpts{
 	Name: "pricefeeder_voting_periods_total",
 	Help: "The total number of voting periods this pricefeeder has participated in",
 })
+
+var PriceSourceLatency = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name:    "pricefeeder_price_source_latency_seconds",
+	Help:    "The latency of querying a price source in seconds",
+	Buckets: prometheus.DefBuckets,
+}, []string{"source"})
