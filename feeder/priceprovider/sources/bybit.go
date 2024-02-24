facc56721b67c6f2af22fa83b7b563a871bcb30f
Kevin Yang <5478483+k-yang@users.noreply.github.com>
2024-02-24 10:59:15 -0700

    refactor: bybit skip unknown symbols

--- feeder/priceprovider/sources/bybit.go
+++ feeder/priceprovider/sources/bybit.go
@@ -2,11 +2,12 @@ package sources
 
 import (
 	"encoding/json"
-	"github.com/NibiruChain/nibiru/x/common/set"
-	"github.com/NibiruChain/pricefeeder/types"
 	"io"
 	"net/http"
 	"strconv"
+
+	"github.com/NibiruChain/nibiru/x/common/set"
+	"github.com/NibiruChain/pricefeeder/types"
 )
 
 const (
@@ -29,7 +30,6 @@ type Response struct {
 // BybitPriceUpdate returns the prices for given symbols or an error.
 // Uses BYBIT API at https://bybit-exchange.github.io/docs/v5/market/tickers.
 func BybitPriceUpdate(symbols set.Set[types.Symbol]) (rawPrices map[types.Symbol]float64, err error) {
-
 	url := "https://api.bybit.com/v5/market/tickers?category=spot"
 
 	resp, err := http.Get(url)
@@ -52,17 +52,16 @@ func BybitPriceUpdate(symbols set.Set[types.Symbol]) (rawPrices map[types.Symbol
 	rawPrices = make(map[types.Symbol]float64)
 
 	for _, ticker := range response.Data.List {
-
 		symbol := types.Symbol(ticker.Symbol)
+		if !symbols.Has(symbol) {
+			continue
+		}
+
 		price, err := strconv.ParseFloat(ticker.Price, 64)
 		if err != nil {
-			return rawPrices, err
+			price = -1
 		}
-
-		if _, ok := symbols[symbol]; ok {
-			rawPrices[symbol] = price
-		}
-
+		rawPrices[symbol] = price
 	}
 	return rawPrices, nil
 }
