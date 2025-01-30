92fe539ef4ca3a3a6425dcba86da41246fae2969
StrathCole <strathcole@gmail.com>
2025-01-30 13:55:29 +0100

    - change stdev

--- feeder/priceprovider/aggregateprovider.go
+++ feeder/priceprovider/aggregateprovider.go
@@ -2,6 +2,8 @@ package priceprovider
 
 import (
 	"encoding/json"
+	"math"
+	"sort"
 
 	"github.com/NibiruChain/nibiru/x/common/asset"
 	"github.com/NibiruChain/pricefeeder/metrics"
@@ -52,12 +54,19 @@ var aggregatePriceProvider = promauto.NewCounterVec(prometheus.CounterOpts{
 func (a AggregatePriceProvider) GetPrice(pair asset.Pair) types.Price {
 	// iterate randomly, if we find a valid price, we return it
 	// otherwise we go onto the next PriceProvider to ask for prices.
+	var allPrices []types.Price
+
 	for _, p := range a.providers {
 		price := p.GetPrice(pair)
 		if price.Valid {
 			aggregatePriceProvider.WithLabelValues(pair.String(), price.SourceName, "true").Inc()
-			return price
+			allPrices = append(allPrices, price)
 		}
+	}
+
+	if len(allPrices) > 0 {
+		finalPrice := a.computeConsolidatedPrice(allPrices, pair)
+		return finalPrice
 	}
 
 	// if we reach here no valid symbols were found
@@ -75,4 +84,73 @@ func (a AggregatePriceProvider) Close() {
 	for _, p := range a.providers {
 		p.Close()
 	}
+}
+
+// computeConsolidatedPrice computes the consolidated price from the given map of prices.
+// it removes outliers and computes the median of the remaining prices.
+func (a AggregatePriceProvider) computeConsolidatedPrice(prices []types.Price, pair asset.Pair) types.Price {
+	if len(prices) == 0 {
+		return types.Price{Price: -1, Pair: pair, SourceName: "missing", Valid: false}
+	}
+	if len(prices) == 1 {
+		return prices[0]
+	}
+	if len(prices) == 2 {
+		avg := (prices[0].Price + prices[1].Price) / 2
+		return types.Price{Price: avg, Pair: pair, SourceName: "consolidated", Valid: true}
+	}
+
+	floatPrices := make([]float64, len(prices))
+	for i, p := range prices {
+		floatPrices[i] = p.Price
+	}
+
+	// remove outliers, then take median
+	cleaned := a.removeOutliers(floatPrices, pair)
+	if len(cleaned) == 0 {
+		return types.Price{Price: -1, Pair: pair, SourceName: "missing", Valid: false}
+	}
+	return types.Price{Price: a.median(cleaned), Pair: pair, SourceName: "consolidated", Valid: true}
+}
+
+// removeOutliers removes outliers from the given prices slice.
+func (a AggregatePriceProvider) removeOutliers(prices []float64, pair asset.Pair) []float64 {
+	mean, stddev := a.meanAndStdDev(prices)
+	var filtered []float64
+	for _, p := range prices {
+		if math.Abs(p-mean) <= 1*stddev { // 2* would be too loose and include too many outliers as valid
+			filtered = append(filtered, p)
+			continue
+		}
+
+		// log outliers
+		a.logger.Warn().Str("pair", pair.String()).Float64("price", p).Float64("mean", mean).Float64("stddev", stddev).Msg("outlier price")
+	}
+	return filtered
+}
+
+// median returns the median of the given prices slice.
+func (a AggregatePriceProvider) median(prices []float64) float64 {
+	sort.Float64s(prices)
+	mid := len(prices) / 2
+	if len(prices)%2 == 1 {
+		return prices[mid]
+	}
+	return (prices[mid-1] + prices[mid]) / 2
+}
+
+// meanAndStdDev returns the mean and standard deviation of the given prices slice.
+func (a AggregatePriceProvider) meanAndStdDev(prices []float64) (float64, float64) {
+	var sum float64
+	for _, p := range prices {
+		sum += p
+	}
+	mean := sum / float64(len(prices))
+	var variance float64
+	for _, p := range prices {
+		diff := p - mean
+		variance += diff * diff
+	}
+	variance /= float64(len(prices) - 1) // sample variance
+	return mean, math.Sqrt(variance)
 }
