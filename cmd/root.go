3961eb5f174429d08eaf20c55ddf018b5f8b759b
james. <86951780+reversesigh@users.noreply.github.com>
2025-02-11 17:06:21 -0500

    feat(metrics): Add custom prometheus port support and log entry (#61)
    * Allow defining custom port for metrics
    
    * Update .env.example
    
    * Add logger.Error for metrics server failure
    
    ---------
    
    Co-authored-by: Kevin Yang <5478483+k-yang@users.noreply.github.com>

--- cmd/root.go
+++ cmd/root.go
@@ -68,10 +68,14 @@ var rootCmd = &cobra.Command{
 
 		handleInterrupt(logger, f)
 
+		metricsPort := os.Getenv("METRICS_PORT")
+		if metricsPort == "" {
+			metricsPort = "8080"
+		}
+		logger.Info().Msgf("Starting metrics server on port %s", metricsPort)
 		http.Handle("/metrics", promhttp.Handler())
-		err := http.ListenAndServe(":8080", nil)
-		if err != nil {
-			logger.Error().Err(err).Msg("failed to start metrics server")
+		if err := http.ListenAndServe(":"+metricsPort, nil); err != nil {
+			logger.Error().Err(err).Msgf("Failed to start metrics server on port %s", metricsPort)
 			os.Exit(1)
 		}
 		select {}
