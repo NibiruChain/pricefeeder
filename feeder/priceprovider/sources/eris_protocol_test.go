532ad6a48f29228a0993bc5ff7171bba95039e7c
Oleg Nikonychev <oleg.nikonychev@gmail.com>
2025-08-08 10:47:11 +0600

    feat: eris protocol (stnibi) custom configuration (#65)
    * feat: eris protocol (stnibi) custom configuration
    
    * chore: cleanup
    
    * fix: tests

--- feeder/priceprovider/sources/eris_protocol_test.go
+++ feeder/priceprovider/sources/eris_protocol_test.go
@@ -12,6 +12,7 @@ import (
 
 func TestErisProtocolPriceUpdate(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
+		t.Setenv("GRPC_READ_ENDPOINT", "grpc.nibiru.fi:443")
 		rawPrices, err := ErisProtocolPriceUpdate(set.New[types.Symbol](), zerolog.New(io.Discard))
 		require.NoError(t, err)
 		require.Equal(t, 1, len(rawPrices))
